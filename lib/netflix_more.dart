b12127b9af70526c6d4753e702ea2f1bb9c58c89
fonkamloic <fonkamloic@gmail.com>
2019-11-26 04:33:15 +0100

    netflix more page

---
+++ lib/netflix_more.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NetflixMore extends StatefulWidget {
+  @override
+  _NetflixMoreState createState() => _NetflixMoreState();
+}
+
+class _NetflixMoreState extends State<NetflixMore> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
