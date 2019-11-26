aeb593091409abcc6473334c7174b42e7a52b913
fonkamloic <fonkamloic@gmail.com>
2019-11-26 04:32:39 +0100

    netflix download page

---
+++ lib/netflix_download.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NetflixDownload extends StatefulWidget {
+  @override
+  _NetflixDownloadState createState() => _NetflixDownloadState();
+}
+
+class _NetflixDownloadState extends State<NetflixDownload> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
